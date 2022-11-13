4ff1d4ec71f721424a9e0542f61de75ef12584cb
Kwon Valerii <wellko24@gmail.com>
2022-11-14 00:00:36 +0600

    copied Modal.tsx from Classwork and added 'close' button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Modal from "./Components/Modal/Modal";
 
 function App() {
-  return (
-    <div className="App">
 
+  const [showModal, setShowModal] = useState(false);
+
+  const cancel = () => setShowModal(false);
+
+
+  return (
+    <div className='container-fluid text-center'>
+      <button
+          className="btn btn-primary"
+          onClick={() => setShowModal(true)}
+      >
+        Call Modal
+      </button>
+      <Modal
+          show={showModal}
+          onClose={cancel}
+          title="Some kinda modal title"
+      >
+        <p>This is modal content</p>
+      </Modal>
     </div>
   );
 }
